836a30596185d8c7a061fca93abe4233271b762c
gnarciso27 <guilhermenarcisotrigueiro@gmail.com>
2024-08-19 13:44:40 -0300

    replace

---
+++ revisao.S.e.A/q4-replaceWord.js
@@ -0,0 +1,14 @@
+function replaceWord(originalString, wordToReplace, replacementWord) {
+    
+    const parts = originalString.split(wordToReplace);
+    
+    
+    return parts.join(replacementWord);
+}
+
+
+console.log(replaceWord("The quick brown fox jumps over the lazy dog", "fox", "cat")); 
+
+
+console.log(replaceWord("Hello world! World is beautiful.", "world", "Earth")); 
+
