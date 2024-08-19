ec1c01d4adfc05416397b87532ee15d55ecda0f3
afonso <156040578+afonsotrs@users.noreply.github.com>
2024-08-19 17:08:13 +0000

    cuzinho gostoso

--- revisao.S.e.A/q4-replaceWord.js
+++ revisao.S.e.A/q4-replaceWord.js
@@ -1,14 +1,8 @@
 function replaceWord(originalString, wordToReplace, replacementWord) {
-    
     const parts = originalString.split(wordToReplace);
-    
-    
     return parts.join(replacementWord);
 }
 
-
 console.log(replaceWord("The quick brown fox jumps over the lazy dog", "fox", "cat")); 
-
-
 console.log(replaceWord("Hello world! World is beautiful.", "world", "Earth")); 
 
---
+++ revisao.S.e.A/q5-findDuplicate.js
@@ -0,0 +1,14 @@
+// Questão 5: Escreva uma função chamada findDuplicate que recebe um array de números
+// inteiros e retorna o primeiro elemento que aparece mais de uma vez. Se não houver
+// elementos duplicados, a função deve retornar null. Por exemplo:
+// findDuplicate([1, 2, 3, 4, 2]); // retorna 2
+// findDuplicate([1, 2, 3, 4, 5]); // retorna null
+
+const findDuplicate = (array) => {
+    let novoarray = []
+    let tamanhoarray = array.length
+    for(let i=0; i<tamanhoarray; i++){
+        let numero = array[i]
+    }
+
+}
