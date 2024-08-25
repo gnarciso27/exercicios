b70f36e9604cf8d255d69aa12589ec5a09f710c5
gnarciso27 <guilhermenarcisotrigueiro@gmail.com>
2024-08-25 13:13:38 -0300

    afonsocu

---
+++ revisao.S.e.A/q14-expandRange.js
@@ -0,0 +1,27 @@
+function expandRange(str){
+
+    if (str.length === 0 || typeof str !== 'string'){
+        return null
+    }
+
+    const arrResult = [];
+    let strParts = str.split(',');
+
+    for(let part of strParts){
+        if (part.includes('-')){
+            let [numStart, numEnd] = part.split('-');
+            numStart = Number(numStart);
+            numEnd = Number(numEnd);
+
+            for(; numStart <= numEnd; numStart++){
+                arrResult.push(numStart)
+            };
+        } else {
+            arrResult.push(Number(part));
+        };
+    };
+
+    return arrResult
+ };
+
+ console.log(expandRange('1-5, 6, 7-12, 13, 14-30'));
