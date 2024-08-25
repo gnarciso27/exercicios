65d9c904832a40b74f21fbabe3d5dfe49b8613cc
gnarciso27 <guilhermenarcisotrigueiro@gmail.com>
2024-08-25 13:10:02 -0300

    13

---
+++ revisao.S.e.A/q13-rotateArray.js
@@ -0,0 +1,12 @@
+function rotatearray(arr, k ){
+    const n = arr.length;
+    if (n === 0) return arr;
+    k = ((k % n) + n) % n;
+    const result = new Array(n);
+    for(let i = 0; i < n; i++){
+        result[(i + k) % n ] = arr[i];
+    }
+    return result;
+}
+console.log(rotatearray([1,2,3,4,5], 2));
+console.log(rotatearray([1,2,3,4,5], -2));
